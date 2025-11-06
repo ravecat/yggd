09ab9a295ec19b03c99cae1260c3b780a7c48c42
Max Sharov <maxsharoff@gmail.com>
2025-11-06 18:39:28 +0100

    fix: disable experimental caching for nextjs

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
-  cacheComponents: true,
+  cacheComponents: false,
 };
 
 const plugins = [
