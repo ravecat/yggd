0fd529c52d67d3fa849dac54c3dd3d05ef4df9b9
Max Sharov <maxsharoff@gmail.com>
2025-11-10 15:37:04 +0100

    refactor: update test environment

---
+++ packages/shared/jest.config.cjs
@@ -0,0 +1,21 @@
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+// Reading the SWC compilation config for the spec files
+const swcJestConfig = JSON.parse(
+  readFileSync(join(__dirname, '.spec.swcrc'), 'utf-8')
+);
+
+// Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
+swcJestConfig.swcrc = false;
+
+module.exports = {
+  displayName: '@yggd/shared',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: 'test-output/jest/coverage'
+};
