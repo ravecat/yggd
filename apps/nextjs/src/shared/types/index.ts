b9cbf5795974c14ee18f25dac8550054f1351e58
Max Sharov <maxsharoff@gmail.com>
2025-11-13 23:43:14 +0100

    feat: integrate Yjs phoenix channel provider to nextjs

--- apps/nextjs/src/shared/types/index.ts
+++ apps/nextjs/src/shared/types/index.ts
@@ -1,1 +1,2 @@
 export type { SearchParams, AsyncSearchParams } from "./query";
+export type { ExcalidrawAPI } from "./excalidraw";
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,4 +2,5 @@ export * from './api/generated/index.js';
 export * from './api/client.js';
 export * from './config/posts.js';
 export * from './lib/query.js';
+export * from './lib/socket.js';
 export * from './types/query.js';
