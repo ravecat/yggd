6366220f22ee3e8556f3d288e8c6edbd0450930e
Max Sharov <maxsharoff@gmail.com>
2025-11-19 18:39:12 +0100

    chore: improve build for nextjs app

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -8,13 +8,14 @@ const { composePlugins, withNx } = require("@nx/next");
 const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
-  nx: {},
+  // nx: {},
   cacheComponents: true,
 };
 
+/** @type {(import("@nx/next/src/utils/config").NextPlugin | import("@nx/next/src/utils/config").NextPluginThatReturnsConfigFn)[]} */
 const plugins = [
   // Add more Next.js plugins to this list if needed.
-  withNx,
+  // withNx,
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
