2360e68c0c370a923aeee4bf0e7af0aa0d4a9bf7
Max Sharov <maxsharoff@gmail.com>
2025-12-03 00:40:59 +0100

    refactor(nextjs): connect to socket based on public env

--- apps/nextjs/src/components/socket-provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { Socket } from "@rvct/shared/react";
-
-export function SocketProvider({ children }: { children: React.ReactNode }) {
-  return <Socket>{children}</Socket>;
-}
