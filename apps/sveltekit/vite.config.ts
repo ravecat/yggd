0e6f2e321ce068dae588c0d1eb4042dcac7b68de
Max Sharov <maxsharoff@gmail.com>
2026-03-11 11:41:47 +0100

    chore(sveltekit): update dev config

--- apps/sveltekit/vite.config.ts
+++ apps/sveltekit/vite.config.ts
@@ -4,6 +4,16 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [sveltekit(), svelteTesting()],
+  server: {
+    host: "0.0.0.0",
+    port: 3001,
+    strictPort: true,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 3001,
+    strictPort: true,
+  },
   test: {
     environment: "jsdom",
     include: ["src/**/*.{spec,test}.{js,ts}", "tests/**/*.{spec,test}.{js,ts}"],
